36f4c2974d47544a8cf564dc8f49d4f474c38798
Joshua Walsh <josh@ymindustries.com>
2023-02-17 22:58:06 +1100

    Replace icon for Only Child

--- client/src/app/panel-layout/panel-types/special-award.ts
+++ client/src/app/panel-layout/panel-types/special-award.ts
@@ -68,7 +68,7 @@ const specialAwards: {[name: string]: AwardProperties} = {
         interest: 1,
     },
     "Only Child": {
-        icon: "/assets/special-award-icons/onlychild.svg",
+        icon: "/assets/special-award-icons/onlychild3.svg",
         interest: 1,
     },
     "Hipster": {
