5a06c63565b330c8372b5fb6ca3526a0230f45d6
Joshua Walsh <josh@ymindustries.com>
2023-04-10 21:43:27 +1000

    Fix off-by-one error in status descriptions

--- client/src/app/opinions/opinions.component.ts
+++ client/src/app/opinions/opinions.component.ts
@@ -261,13 +261,13 @@ export class OpinionsComponent implements OnInit {
 			return `Loading required anime... (${this.pendingJob.totalAnime - this.pendingJob.remainingAnime} / ${this.pendingJob.totalAnime})` +
 			(
 				this.pendingJob.now < this.pendingJob.queued ?
-				` Your last required anime is #${(this.pendingJob.animeQueuePosition as number) + 1} in the queue.` :
+				` Your last required anime is #${Math.max(this.pendingJob.animeQueuePosition as number, 1)} in the queue.` :
 				''
 			);
 		}
 
 		if(this.pendingJob.now < this.pendingJob.processingStarted) {
-			return `All required anime loaded, queued for processing. Your job is #${(this.pendingJob.jobQueuePosition as number) + 1} in the queue.`;
+			return `All required anime loaded, queued for processing. Your job is #${Math.max(this.pendingJob.jobQueuePosition as number, 1)} in the queue.`;
 		}
 
 		if(this.pendingJob.now < this.pendingJob.completed) {
