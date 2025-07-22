cdfe8ec75a773a45ea298e3d3a9cd4c718d6a5d7
Yoshi Walsh <yoshi@yoshiwalsh.me>
2025-07-22 17:12:59 +1000

    Fix generated image sometimes having incorrect height

--- client/src/app/dom-image/dom-image.component.ts
+++ client/src/app/dom-image/dom-image.component.ts
@@ -20,16 +20,17 @@ export class DomImageComponent implements OnInit {
 	public async renderImage(): Promise<string> {
 		await delay(0);
 
-		const bounding = this.canvasElm.nativeElement.getBoundingClientRect();
 
 		try {
+			const tempBounding = this.canvasElm.nativeElement.getBoundingClientRect();
 			const discard = await domtoimage.toPng(this.canvasElm.nativeElement as Element, {
 				quality: 100,
-				width: bounding.width,
-				height: bounding.height,
+				width: tempBounding.width,
+				height: tempBounding.height,
 			});
 			// We render the image twice in order to work around a bug in Firefox. (webcompat/web-bugs#119834)
 			// The second render seems to be quick anyway, so this is a small price to pay.
+			const bounding = this.canvasElm.nativeElement.getBoundingClientRect();
 			const png = await domtoimage.toPng(this.canvasElm.nativeElement as Element, {
 				quality: 100,
 				width: bounding.width,
